308781264abb422bb9aef577fa4aac9ad1635f74
Frank R Thomsen <frank.thomsen@gmail.com>
2022-06-29 11:23:17 +0200

    hold on a minute

--- utils/copy_assets.sh
+++ utils/copy_assets.sh
@@ -2,17 +2,7 @@
 
 # Copies files to a given location
 
-echo "Installing design-system-icons will copy icon files to your project."
-echo "Choose a directory for the icons [/assets/icons]:"
-read DIR
-echo "You picked $DIR"
+echo "Copying files to /assets/icons"
+mkdir assets/icons
+cp icons/*.svg assets/icons
 
-if [$DIR != ""]
-then
-  echo "Copying files to $DIR"
-  cp icons/*.svg $DIR
-else
-  echo "Copying files to /assets/icons"
-  mkdir mytemp
-  cp icons/*.svg mytemp/
-fi
